7243bf20fc5ddc840de14bf7ce225779a4b4af14
CharlottedeB <32928935+CharlottedeB@users.noreply.github.com>
2018-03-22 14:25:26 -0700

    Images look like experiment

--- analysisRTs.R
+++ analysisRTs.R
@@ -10,7 +10,7 @@ aggregate(session2Mono$EncodingRT,list(session2Mono$Practiced),mean)
 aggregate(session2Mono$ExecutionRT,list(session2Mono$Practiced),mean)
 
 aggregate(session2Bi$EncodingRT,list(session2Bi$Practiced),mean)
-aggregate(session2Bi$ExecutionRT,list(session2Bi$Practiced),mean)
+aggregate(session2Bi$ExecutionRT,list(session2Bi$Practiced),mean)$x*1000
 
 
 library(ggplot2)
@@ -19,11 +19,11 @@ session2Bi$lang <- "Bilingual"
 merged <- rbind(session2Mono,session2Bi)
 ggplot(merged, aes(lang,EncodingRT*1000,fill=Practiced)) + 
   geom_bar(stat = "summary", fun.y=mean,position = "dodge") + 
-  scale_fill_grey(breaks=c(FALSE,TRUE), labels=c("Novel", "Practiced"), name="") +
+  scale_fill_grey(start= 0.8, end = 0.2, breaks=c(FALSE,TRUE), labels=c("Novel", "Practiced"), name="") +
   theme_bw() + 
-  labs(title = "Encoding Times", y = "Response Time (ms)", x ="")
+  labs(title = "Encoding Times", y = "Response Time (ms)", x ="") + ylim(0,5000)
 ggplot(merged, aes(lang,ExecutionRT*1000,fill=Practiced)) +
   geom_bar(stat = "summary", fun.y=mean,position = "dodge") + 
-  scale_fill_grey(breaks=c(FALSE,TRUE), labels=c("Novel", "Practiced"), name="") +
+  scale_fill_grey(start= 0.8, end = 0.2, breaks=c(FALSE,TRUE), labels=c("Novel", "Practiced"), name="") +
   theme_bw() + 
-  labs(title = "Execution Times", y = "Response Time (ms)", x ="")
+  labs(title = "Execution Times", y = "Response Time (ms)", x ="") + ylim(0,5000)
