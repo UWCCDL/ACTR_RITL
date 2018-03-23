003e5032927f88022d9ee723cb765cb42554b46d
TheRealDrDre <stocco@uw.edu>
2018-03-23 16:31:56 -0700

    New mono model

--- analysisRTs.R
+++ analysisRTs.R
@@ -1,10 +1,10 @@
-modelMonolingual <- read.csv("~/Documents/GitHub/ACTR_RITL/modelMonolingual.txt")
+modelMonolingual <- read.csv("model-temporal.txt", header=T, )
 session2Mono <- modelMonolingual[21:60,]
 session2Mono$Practiced <- session2Mono$Rule == "(INCREMENT DOUBLE DIVIDE)" | session2Mono$Rule == "(TRIPLE INCREMENT ADD)"
 
-modelBilingual <- read.csv("~/Documents/GitHub/ACTR_RITL/modelBilingual.txt")
-session2Bi <- modelBilingual[21:60,]
-session2Bi$Practiced <- session2Bi$Rule == "(INCREMENT DOUBLE DIVIDE)" | session2Bi$Rule == "(TRIPLE INCREMENT ADD)"
+#modelBilingual <- read.csv("~/Documents/GitHub/ACTR_RITL/modelBilingual.txt")
+#session2Bi <- modelBilingual[21:60,]
+#session2Bi$Practiced <- session2Bi$Rule == "(INCREMENT DOUBLE DIVIDE)" | session2Bi$Rule == "(TRIPLE INCREMENT ADD)"
 
 aggregate(session2Mono$EncodingRT,list(session2Mono$Practiced),mean)
 aggregate(session2Mono$ExecutionRT,list(session2Mono$Practiced),mean)
