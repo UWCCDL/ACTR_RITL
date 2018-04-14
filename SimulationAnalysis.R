2240a8c4104dbe3ea598e183d98b45b1c1116b51
Charlotte de Blecourt <charlotte@ceres.ilabs.uw.edu>
2018-04-13 17:59:47 -0700

    Get parameters

--- SimulationAnalysis.R
+++ SimulationAnalysis.R
@@ -102,88 +102,34 @@ hist(deltaMonoExecution)
 
 
 ## Read experiment data
-
 fileList <- list.files("/projects/actr/models/ACTR_RITL/RITLExperimentData/", pattern=".txt")
 DTExperiment <- rbindlist( sapply(paste("/projects/actr/models/ACTR_RITL/RITLExperimentData/", fileList, sep=""), fread, simplify = FALSE),
                    use.names = TRUE, idcol = "FileName")
 DTExperiment <- DTExperiment[DTExperiment$Execution.RT != 0 | DTExperiment$Encoding.RT != 0,] # Remove rts of 0 ms
 DTExperiment <- DTExperiment[,-c("Procedure","Running")]
-subjects <- read.table("/projects/BBT/RITL/groups_version9.txt")
-DTExperiment <- merge(DTExperiment,subjects, by.x="Subject",by.y = "V1")
+subjects <- read.table("/projects/BBT/RITL/groups_version8.txt")  #Has all subject names (but V9 also exists??)
+DTExperiment <- merge(DTExperiment,subjects, by.x="Subject",by.y = "V1", all =T)
 
-# Aggregate by trial number, practiced, and language OOPS DONT NEED THIS
+# Aggregate by trial number, practiced, and language
 experimentEnc <- aggregate(DTExperiment$Encoding.RT, by = list(DTExperiment$Trials, DTExperiment$Practiced, DTExperiment$V2), mean)
 experimentEx <- aggregate(DTExperiment$Execution.RT, by = list(DTExperiment$Trials, DTExperiment$Practiced, DTExperiment$V2), mean)
 
 ## Attempt to get error
-
 error <- function(simulations, experiment) {
-  simulations$error <- sqrt((simulations$RT - (experiment/1000))**2)
-  subset(simulations, simulations$error == min(simulations$error))
+  simulations$error <- sqrt((simulations$RT - (experiment/1000))**2) # Get error per observation, square and sqrt
+  byParams <- aggregate(simulations$error, by = list(simulations$alpha, simulations$ans, simulations$`imaginal-delay`,simulations$le,simulations$nu), mean) #Get mean error per set of parameters
+  subset(byParams, byParams$x == min(byParams$x)) # get parameter set with least error
 }
 
-simulations <- experimentEnc[(experimentEnc$Group.2 == "No") & (experimentEnc$Group.3 == "Bilingual"),]
+error(biNovelEnc, experimentEnc$x[(experimentEnc$Group.2 == "No") & (experimentEnc$Group.3 == "Bilingual")])
+error(biPracticedEnc, experimentEnc$x[(experimentEnc$Group.2 == "Yes") & (experimentEnc$Group.3 == "Bilingual")])
 
-error(biNovelEnc,experimentEnc$x[(experimentEnc$Group.2 == "No") & (experimentEnc$Group.3 == "Bilingual")])
-error(biPracticedEnc,experimentEnc$x[(experimentEnc$Group.2 == "Yes") & (experimentEnc$Group.3 == "Bilingual")])
+error(biNovelEx,experimentEx$x[(experimentEx$Group.2 == "No") & (experimentEx$Group.3 == "Bilingual")])
+error(biPracticedEx,experimentEx$x[(experimentEx$Group.2 == "Yes") & (experimentEx$Group.3 == "Bilingual")])
 
-error(biNovelEx,experimentEx$x[(experimentEx$Group.1 == "No") & (experimentEx$Group.2 == "Bilingual")])
-error(biPracticedEx,experimentEx$x[(experimentEx$Group.1 == "Yes") & (experimentEx$Group.2 == "Bilingual")])
+error(monoNovelEnc,experimentEnc$x[(experimentEnc$Group.2 == "No") & (experimentEnc$Group.3 == "Monolingual")])
+error(monoPracticedEnc,experimentEnc$x[(experimentEnc$Group.2 == "Yes") & (experimentEnc$Group.3 == "Monolingual")])
 
-error(monoNovelEnc,experimentEnc$x[(experimentEnc$Group.1 == "No") & (experimentEnc$Group.2 == "Monolingual")])
-error(monoPracticedEnc,experimentEnc$x[(experimentEnc$Group.1 == "Yes") & (experimentEnc$Group.2 == "Monolingual")])
-
-error(monoNovelEx,experimentEx$x[(experimentEx$Group.1 == "No") & (experimentEx$Group.2 == "Monolingual")])
-error(monoPracticedEx,experimentEx$x[(experimentEx$Group.1 == "Yes") & (experimentEx$Group.2 == "Monolingual")])
-
-## Error function 1: Minimize distance from the four base values
-error <- function(a, b, c, d) {
-  ta <- target[1]
-  tb <- target[2]
-  tc <- target[3]
-  td <- target[4]
-  
-  vec <- 100 * c(mean(c(a,b)), mean(c(c,d)), a - b, c - d)
-  tvec <- 100 * c(mean(c(ta,tb)), mean(c(tc,td)), ta - tb, tc - td)
-  
-  sum((vec - tvec) ** 2)
-}
-
-verror <- Vectorize(error)
-
-equal$Error <- verror(equal$Con_ACC, equal$Incon_ACC,
-                      equal$Con_RT, equal$Incon_RT)
-
-
-
-## Error function 2: Minimize distance from congruent conditions
-## and effects.
-error2 <- function(a, b, c, d) {
-  ta <- target[1]
-  tb <- target[2]
-  tc <- target[3]
-  td <- target[4]
-  
-  vec <- 100 * c(a, c, a - b, c - d)
-  tvec <- 100 * c(ta, tc, ta - tb, tc - td)
-  
-  sum((vec - tvec) ** 2)
-}
-
-
-verror2 <- Vectorize(error2)
-
-equal$Error2 <- verror2(equal$Con_ACC, equal$Incon_ACC,
-                        equal$Con_RT, equal$Incon_RT)
-
-
-# Both criteria converge on the same set of parameters
-model <- subset(equal, equal$Error == min(equal$Error))
-model <- subset(equal, equal$Error2 == min(equal$Error2))
-
-
-
-# Generates a random factor
-model$Run <- paste("Run", 1:dim(model)[1])
-
+error(monoNovelEx,experimentEx$x[(experimentEx$Group.2 == "No") & (experimentEx$Group.3 == "Monolingual")])
+error(monoPracticedEx,experimentEx$x[(experimentEx$Group.2 == "Yes") & (experimentEx$Group.3 == "Monolingual")])
 
