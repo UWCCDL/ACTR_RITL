f165245878af08b7d0232ea883e50515fc9192f0
CharlottedeB <32928935+CharlottedeB@users.noreply.github.com>
2018-09-11 18:01:15 +0200

    Files for MATLAB analysis
    Only monolingual for now. model_act_subject_trialType_trial Perhaps remove practice trials

---
+++ ACTR_BOLD/createTrialFiles.R
@@ -0,0 +1,40 @@
+# Read in ACT-R file
+model_act <- read.table("~/GitHub/ACTR_RITL/ACTR_BOLD/model_act.txt", quote="\"", comment.char="")
+
+# Use Manual module to get trial times
+# Manual module is used 3x in one trial -> after encoding, execution and probe
+manual <- model_act[model_act$V1 == "MANUAL",]
+manual$phase <- rep(c("encoding","execution","probe"), 60)
+TrialTime <- data.frame(endTime = manual$V3[manual$phase == "probe"], trial = 1:60)
+
+# Get trial numbers in ACT-R var
+model_act$trial <- NA
+
+model_act$trial[model_act$V3 <= TrialTime[1,1]] <- TrialTime[1,2] # first trial
+
+for (i in 2:nrow(TrialTime)) {
+  model_act$trial[(model_act$V3 <= TrialTime[i,1]) & (model_act$V3 > TrialTime[i-1,1])] <- TrialTime[i,2]
+}
+
+model_act$trial[is.na(model_act$trial)] <- tail(TrialTime,1)[1,2] # Remaining NAs belong to last trial
+
+# Get model behavior
+model <- read.csv("~/GitHub/ACTR_RITL/ACTR_BOLD/model.txt")
+model$trial <- 1:60
+model$type <- NA
+
+# Determine rules, and then trial types
+rules <- unique(model$Rule[1:20])
+model$type[model$Rule %in% unique(model$Rule[1:20])] <- 2 #Practiced instructions
+model$type[1:20] <- 1 # Training
+model$type[is.na(model$type)] <- 3 # novel instructions
+
+dat <- merge(model_act,model[,c("trial","type")], sort = F)
+
+# Write files for MATLAB
+for (i in 1:max(dat$trial)) {
+  temp <- dat[dat$trial == i,c("V1","V2","V3")]
+  type <- unique(dat$type[dat$trial == i])
+  
+  write.table(dat,paste("model_act_1_", type, "_", i, ".txt",sep = ""), col.names = F, row.names = F) #model_act_subject_type_trial.txt
+}
